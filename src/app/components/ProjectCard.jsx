59e6c3ebcce6830b33ce47e5bf5a87b128ff778f
Quinny <472518620@qq.com>
2024-07-21 11:46:43 +0800

    0721

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -6,10 +6,11 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72 rounded-t-xl relative group"
-        style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
+        className="h-72 md:h-96 w-full rounded-t-xl relative group flex justify-center"
+        // style={{ background: `url(${imgUrl})`, backgroundSize: "cover"}}
       >
-        <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
+        <img className="max-w-9/12 md:max-w-7/12 object-cover" src={imgUrl} alt="" />
+        {/* <div className="overlay items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500 ">
           <Link
             href={gitUrl}
             className="h-14 w-14 mr-2 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
@@ -18,13 +19,13 @@ const ProjectCard = ({ imgUrl, title, description, gitUrl, previewUrl }) => {
           </Link>
           <Link
             href={previewUrl}
-            className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
+            className="h-14 w-14 border-2 reative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <EyeIcon className="h-10 w-10 text-[#ADB7BE] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2  cursor-pointer group-hover/link:text-white" />
           </Link>
-        </div>
+        </div> */}
       </div>
-      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4">
+      <div className="text-white rounded-b-xl mt-3 bg-[#181818]py-6 px-4 text-center">
         <h5 className="text-xl font-semibold mb-2">{title}</h5>
         <p className="text-[#ADB7BE]">{description}</p>
       </div>
---
+++ src/app/projects/layout.js
@@ -0,0 +1,8 @@
+
+export default function ProjectPage({ children }) {
+    return (
+      <main className="flex min-h-screen flex-col bg-[#F6F4F6]">
+        {children}
+      </main>
+    );
+  }
